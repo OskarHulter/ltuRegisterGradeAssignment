fdf1f8898e20e677ab523f2d37ec0d2da433c085
Stresslessness <stresslessness91@gmail.com>
2017-12-06 22:42:11 +0100

    Äntligen fått det att fungera! Återstår bara polering av UI

--- ltuRegisterGradeAssignment/public/js/httpRequester.js
+++ ltuRegisterGradeAssignment/public/js/httpRequester.js
@@ -1,5 +1,7 @@
 const provtillfalleContainer = document.getElementById("provtillfallen-info");
 const btn = document.getElementById("btn");
+const form = document.getElementById("betyg-form");
+const betygResultat = document.getElementById("betyg-resultat");
 
 btn.addEventListener("click", function () {
     let request = new XMLHttpRequest();
@@ -11,15 +13,41 @@ btn.addEventListener("click", function () {
     request.send();
 });
 
+form.onsubmit = function(e) {
+    e.preventDefault();
+    let provtillfalleId = form.provTillfalleId.value;
+    let putUrl = "http://localhost:4000/api/provtillfallen/" + provtillfalleId; 
+    let betyg = form.rbGrades.value;
+    let betygsatt = true;
+
+    let betygRequest = new XMLHttpRequest();
+    betygRequest.open('PUT', putUrl, true);
+    betygRequest.setRequestHeader('Content-Type', 'application/json');
+    betygRequest.onload = function () {
+        if (betygRequest.status === 200) {
+            let userInfo = JSON.parse(betygRequest.responseText);
+        }
+    };
+    betygRequest.send(JSON.stringify({
+        "betyg": betyg,
+        "betygsatt": betygsatt
+    }));
+    
+    betygResultat.innerHTML = 'Du gav ett betyg!' + form.rbGrades.value;
+    form.reset();
+}
+
 function renderHTML(data) {
     let htmlString = "";
 
     for (i = 0; i < data.length; i++) {
         if (data[i].betygsatt === false) {
         htmlString += "<p>" +
-        data[i].datum + " " +
+        data[i]._id + " " +
+        data[i].prov.provKod + " " +
         data[i].student.namn + " " +
         data[i].student.pnr + " " +
+        data[i].student.studentId + " " +
         data[i].kurs.kursKod + " " +
         data[i].kurs.namn + ".</p>";
         }
