c790e8e2f2affd2739d185e56daadd2efcadbbbb
Stresslessness <stresslessness91@gmail.com>
2017-12-07 00:45:06 +0100

    Fixade bugg med Delete requesten och tog bort gammal kod.

--- ltuRegisterGradeAssignment/routes/api.js
+++ ltuRegisterGradeAssignment/routes/api.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-//const Student = require('../models/student');
 const ProvTillfalle = require('../models/provTillfalle');
 
 //Hämtar en lista över provTillfallen från db
@@ -16,6 +15,7 @@ router.post('/provtillfallen', function(req, res, next){
         res.send(provTillfalle);
     }).catch(next);
 });
+
 //Uppdaterar ett provTillfalle i db
 router.put('/provtillfallen/:id', function(req, res, next){
     ProvTillfalle.findByIdAndUpdate({_id: req.params.id}, req.body, {new:true}).then(function(provTillfalle){
@@ -25,37 +25,9 @@ router.put('/provtillfallen/:id', function(req, res, next){
 
 //Tar bort provTillfalle från db
 router.delete('/provtillfallen/:id', function(req, res, next){
-    Student.findByIdAndRemove({_id: req.params.id}).then(function(student){
-        res.send(student);
+    ProvTillfalle.findByIdAndRemove({_id: req.params.id}).then(function(provTillfalle){
+        res.send(provTillfalle);
     });
 });
 
-/*
-//get a list of students from the db
-router.get('/students', function(req, res, next){
-    Student.find({}).then(function(students){
-        res.send(students);
-    });
-});
-
-//add a new student to the db
-router.post('/students', function(req, res, next){
-    Student.create(req.body).then(function(student){
-        res.send(student);
-    }).catch(next);
-});
-//update a student in the db
-router.put('/students/:id', function(req, res, next){
-    Student.findByIdAndUpdate({_id: req.params.id}, req.body, {new:true}).then(function(student){
-        res.send(student);
-    })
-});
-
-//delete a student from the db
-router.delete('/students/:id', function(req, res, next){
-    Student.findByIdAndRemove({_id: req.params.id}).then(function(student){
-        res.send(student);
-    });
-});
-*/
 module.exports = router;
