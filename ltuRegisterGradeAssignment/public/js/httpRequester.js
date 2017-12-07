0f40db7cc551a2736c64d0f8ec39998edf87e5f0
Stresslessness <stresslessness91@gmail.com>
2017-12-07 02:03:41 +0100

    Omfaktorerat vyn, WIP

--- ltuRegisterGradeAssignment/public/js/httpRequester.js
+++ ltuRegisterGradeAssignment/public/js/httpRequester.js
@@ -3,20 +3,10 @@ const btn = document.getElementById("btn");
 const form = document.getElementById("betyg-form");
 const betygResultat = document.getElementById("betyg-resultat");
 
-btn.addEventListener("click", function () {
-    let request = new XMLHttpRequest();
-    request.open('GET', "http://localhost:4000/api/provtillfallen", true);
-    request.onload = function () {
-        let responseData = JSON.parse(request.responseText);
-        renderHTML(responseData);
-    };
-    request.send();
-});
-
-form.onsubmit = function(e) {
+form.onsubmit = function (e) {
     e.preventDefault();
     let provtillfalleId = form.provTillfalleId.value;
-    let putUrl = "http://localhost:4000/api/provtillfallen/" + provtillfalleId; 
+    let putUrl = "http://localhost:4000/api/provtillfallen/" + provtillfalleId;
     let betyg = form.rbGrades.value;
     let betygsatt = true;
 
@@ -32,26 +22,44 @@ form.onsubmit = function(e) {
         "betyg": betyg,
         "betygsatt": betygsatt
     }));
-    
+
     betygResultat.innerHTML = 'Du gav ett betyg!' + form.rbGrades.value;
     form.reset();
+    getProvListaUtanBetyg();
+}
+
+btn.addEventListener("click", function () {
+    getProvListaUtanBetyg();
+});
+
+function getProvListaUtanBetyg() {
+    let request = new XMLHttpRequest();
+    request.open('GET', "http://localhost:4000/api/provtillfallen", true);
+    request.onload = function () {
+        let responseData = JSON.parse(request.responseText);
+        renderHTML(responseData);
+    };
+    request.send();
 }
 
 function renderHTML(data) {
     let htmlString = "";
+    clearElement("provtillfallen-info");
 
     for (i = 0; i < data.length; i++) {
         if (data[i].betygsatt === false) {
-        htmlString += "<p>" +
-        data[i]._id + " " +
-        data[i].prov.provKod + " " +
-        data[i].student.namn + " " +
-        data[i].student.pnr + " " +
-        data[i].student.studentId + " " +
-        data[i].kurs.kursKod + " " +
-        data[i].kurs.namn + ".</p>";
+            htmlString += "<p>" +
+                data[i].student.studentId + " " +
+                data[i].student.namn + " " +
+                data[i].kurs.kursKod + " " +
+                data[i].kurs.namn + " " +
+                "Provkod:" + data[i].prov.provKod + " " +
+                "<strong>Provtillfällets ID:</strong>" + data[i]._id + "</p>";
         }
     }
-
     provtillfalleContainer.insertAdjacentHTML('beforeend', htmlString);
 };
+
+function clearElement(elementId){
+    document.getElementById(elementId).innerHTML = "";
+}
